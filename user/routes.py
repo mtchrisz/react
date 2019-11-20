3a8b90b5f1030ea0a0c43ee4605a4da35ae5c8dd
Christian Zou <mtchrisz@gmail.com>
2019-11-20 07:54:14 +0000

    Support PUT endpoint for adding users

--- user/routes.py
+++ user/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from db import mysql
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 
 users = Blueprint('users', __name__, url_prefix='/users')
 
@@ -16,6 +16,7 @@ def get():
     for (id, name, username, email, phone, website, street, suite, city, zipcode, company) in result:
         user = {
             'id': id,
+            'name': name,
             'username': username,
             'email': email,
             'phone': phone,
@@ -40,7 +41,42 @@ def delete():
 
 @users.route('/', methods=['PUT'])
 def put():
-    pass
+
+    user = request.json
+
+    name = user.get("name", None)
+    username = user.get("username", None)
+    email = user.get("email", None)
+    phone = user.get("phone", None)
+    website = user.get("website", None)
+    street = user.get("street", None)
+    suite = user.get("suite", None)
+    city = user.get("city", None)
+    zipcode = user.get("zipcode", None)
+    company = user.get("company", None)
+
+    db = mysql.get_db()
+    cursor = db.cursor()
+    cursor.execute("""insert into users (name, username, email, phone, website, street, suite, city, zipcode, company) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (name, username, email, phone, website, street, suite, city, zipcode, company))
+    db.commit()
+
+    return jsonify({
+        'id': cursor.lastrowid,
+        'name': name,
+        'username': username,
+        'email': email,
+        'phone': phone,
+        'website': website,
+        'address': {
+            'street': street,
+            'suite': suite,
+            'city': city,
+            'zipcode': zipcode
+        },
+        'company': {
+            'name': company
+        }
+    })
 
 @users.route('/', methods=['POST'])
 def post():
